652da10ee63d21fd6fc70250afbe093fa940a1b4
Vlad <frontx25@gmail.com>
2018-10-06 18:46:54 +0300

    автослайдер

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-document.getElementById('slider-left').onclick = sliderleft;
-var left=0;
+// document.getElementById('slider-left').onclick = sliderleft;
+autoSlider();
+var left = 0;
+var timer;
 
-function sliderleft() {
-    var line = document.getElementById('line');
-    left = left -128;
-    if(left < -512) {
-        left = 0;
-    }
-    line.style.left = left + 'px';
+
+
+function autoSlider () {
+    timer = setTimeout(function(){
+        var line = document.getElementById('line');
+        left = left -128;
+        if(left < -512) {
+            left = 0;
+            clearTimeout (timer);
+        }
+        line.style.left = left + 'px';
+        autoSlider ();
+    }, 1000) ;
 }
